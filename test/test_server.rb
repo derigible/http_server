202a4f729a95e69b56da91c6489e9472200e6fa1
Marc Phillips <marc@crowdstorage.com>
2020-10-08 23:06:32 -0600

    Fix broken test
    TIL - double quotes will encode newlines properly
    in ruby while single quotes escapes them.

--- test/test_server.rb
+++ test/test_server.rb
@@ -22,13 +22,12 @@ class TestServer < HttpServerTest
   end
 
   def test_server_responds_to_http_11_call
-    response = make_request 'GET / HTTP/1.1\n\n'
+    response = make_request "GET / HTTP/1.1\n\n"
     assert response == "HTTP/1.1 200 OK\n"
   end
 
   def test_server_rejects_bad_http_11_call
-    response = make_request 'GET / a HTTP/1.1\n\n'
-    debugger
-    assert response == 'HTTP/1.1 400 BadRequest\n'
+    response = make_request "GET / a HTTP/1.1\n\n"
+    assert response == "HTTP/1.1 400 BadRequest\n"
   end
 end
